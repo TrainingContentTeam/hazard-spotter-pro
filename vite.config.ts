cca2dfeb4cc4a2f4c592e7ea92de5f5260b573d8
TrainingContentTeam <jashley@lexipol.com>
2026-02-12 12:26:47 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ // vite.config.js
+export default defineConfig(() => {
+  return {
+    base: '/',
+  };
+}); }) => ({
   server: {
     host: "::",
     port: 8080,
